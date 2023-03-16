40a3103f37e60bc21a9688df31fc12f9877e6013
Archer028 <obinnaonyema96@yahoo.com>
2023-03-16 08:48:53 -0700

    Task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 
 /**
 *main - prints the phrase Programming is like building a multilingual puzzle
-*retur: returns 0 if exited properly
+*return: 0 if exited properly
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
