bd61aa6bee25a9cab09aa586c762dec4b267e172
Archer028 <obinnaonyema96@yahoo.com>
2023-03-16 21:30:10 -0700

    Task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+*main - main entry of the program
+*Return: 0
+*/
+
+int main(void)
+{
+printf("size of char %zu\n", sizeof(char));
+printf("size of int %zu\n", sizeof(int));
+printf("size of a long int %zu\n", sizeof(long int));
+printf("size of a long long int %zu\n", sizeof(long long int));
+printf("size of a float %zu\n", sizeof(float));
+return (0);
+}
