097b1ce4b82e2175939406a2277d7138132ddd34
Archer028 <obinnaonyema96@yahoo.com>
2023-03-19 02:42:23 -0700

    commit for task 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main: entry point of the program
+*Return - 0
+*/
+
+int main(void)
+{
+int n;
+
+if ( n > 0)
+{
+printf("is positive");
+}
+
+else if (n == 0)
+{
+printf("is zero");
+}
+
+else
+{
+printf("is negative\n");
+}
+return (0);
+}
