2c87c9be37d607107712d064a7cf115014c52d7d
Archer028 <obinnaonyema96@yahoo.com>
2023-03-20 10:31:55 -0700

    commit for task 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+*main -  prints all single digit numbers
+* of base 10 starting from 0, followed
+* by a new line
+* Return: always 0 (success)
+*/
+
+int main(void)
+{
+int i;
+for (i = 18; i < 28; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
