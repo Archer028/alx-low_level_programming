3b9b3f14f1c9a24295a31539944b7070278ea84f
Archer028 <obinnaonyema96@yahoo.com>
2023-03-20 11:09:50 -0700

    commit for task 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main -  prints all single digit numbers
+* of base 10 starting from 0, followed
+* by a new line
+* Return: always 0 (success)
+*/
+int main(void)
+{
+int i,y;
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+for (y = 'a'; y <= 'f'; y++)
+{
+putchar(y);
+}
+putchar('\n');
+return (0);
+}
