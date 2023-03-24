d9279a116d3798adc46af06459af082a886beea3
Archer028 <obinnaonyema96@yahoo.com>
2023-03-24 15:01:42 -0700

    Task

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,15 @@
  * print_numbers - prints number 0 to 9
  * Return: 0
  */
+
 void print_numbers(void)
 {
 	int a = 0;
 
 	while (a <= 9)
-{
-	_putchar(a + '0');
-	a++;
-}
+	{
+		_putchar(a + '0');
+		a++;
+	}
 	_putchar('\n');
 }
