36eba93305dfdfc70249bafb4ef35061c5348b4f
Obinna Theophilus <123469352+Archer028@users.noreply.github.com>
2023-03-28 09:32:38 -1200

    Task

---
+++ 1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps the values of two integers
+ * @a: variable a
+ * @b: variable b
+ * Return: Always 0 (sucess)
+ */
+
+void swap_int(int *a, int *b)
+{
+int c = *a;
+*a = *b;
+*b = c;
+}
