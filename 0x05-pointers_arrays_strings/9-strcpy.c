dc0c7e98da1ba0c41b309e075377fbbc1d44737b
Obinna Theophilus <123469352+Archer028@users.noreply.github.com>
2023-03-30 04:50:36 +0100

    Update 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,7 +11,7 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int len, j;
 
 	len = 0;
 
@@ -20,11 +20,11 @@ char *_strcpy(char *dest, char *src)
 		len++;
 	}
 
-	for (i = 0; i < len; i++)
+	for (j = 0; j < len; j++)
 	{
-		dest[i] = src[i];
+		dest[j] = src[j];
 	}
-	dest[i] = '\0';
+	dest[j] = '\0';
 
 	return (dest);
 }
